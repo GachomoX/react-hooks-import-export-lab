175a02f02f79eec834eefc2908d4b3d9b2c721f0
Dennis <dennisgachomo@gmail.com>
2022-09-21 20:31:23 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+//import {userName , city} from '../data/user'
+import {username , city} from '../data/user'
 
 function Home() {
   return (
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home;
